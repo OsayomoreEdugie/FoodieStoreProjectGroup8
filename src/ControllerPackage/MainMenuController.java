83ea113ed80a5103c60439668e98bbbe4fcdbacc
AkwukwaegbuNneamakaClara <nneamaka_clara.akwukwaegbu0001@stud.hkr.se>
2020-05-25 17:03:06 +0200

    Main menu controller

---
+++ src/ControllerPackage/MainMenuController.java
@@ -0,0 +1,29 @@
+package ControllerPackage;
+
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainMenuController extends Control {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    private void handleDrinksViewButton() {
+        newInterfaceChanges("Drinks");
+    }
+
+    @FXML
+    private void handleFoodsViewButton() {
+        newInterfaceChanges("Food");
+    }
+
+
+
+}
+
