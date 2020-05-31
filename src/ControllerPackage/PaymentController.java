7e02d129717988a36331f83f31bec5dd13339881
OsayomoreEdugie <eosayomore@yahoo.com>
2020-05-31 13:56:26 +0200

    payment Controller Update with pdf method

--- src/ControllerPackage/PaymentController.java
+++ src/ControllerPackage/PaymentController.java
@@ -3,26 +3,29 @@ package ControllerPackage;
 import DatabaseConnection.DbConnector;
 import DatabaseConnection.DbQueries;
 import Table.CartTableModel;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.print.PageLayout;
-import javafx.print.Printer;
-import javafx.print.PrinterJob;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
-import javafx.scene.transform.Scale;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import javax.swing.*;
+import java.io.FileOutputStream;
 import java.net.URL;
 import java.sql.*;
 import java.util.ResourceBundle;
@@ -31,9 +34,12 @@ public class PaymentController extends DbConnector implements Initializable {
 
     public Label subtotalLabel;
     public Label subtotalLabel1;
+    public AnchorPane cartPane;
     DbQueries dbQueries = new DbQueries();
     Control control;
     ObservableList<CartTableModel> oblist = FXCollections.observableArrayList();
+    String logInuser;
+    String getOrderId;
     @FXML
     private Button confirmationButton;
     @FXML
@@ -48,7 +54,6 @@ public class PaymentController extends DbConnector implements Initializable {
     private Statement statement;
     private ResultSet resultSet;
 
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
@@ -57,7 +62,7 @@ public class PaymentController extends DbConnector implements Initializable {
 //
 //            this.statement = connection.createStatement();
         } catch (Exception e) {
-           JOptionPane.showMessageDialog(null,e+"");
+            JOptionPane.showMessageDialog(null, e + "");
         }
 
         populateCartTable();
@@ -83,7 +88,7 @@ public class PaymentController extends DbConnector implements Initializable {
                 }
             }
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null,e);
+            JOptionPane.showMessageDialog(null, e);
         }
         Name.setCellValueFactory(new PropertyValueFactory<>("Name"));
         Quantitty.setCellValueFactory(new PropertyValueFactory<>("Quantitty"));
@@ -101,20 +106,21 @@ public class PaymentController extends DbConnector implements Initializable {
                 }
             }
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null,e+"");
+            JOptionPane.showMessageDialog(null, e + "");
         }
     }
 
     public void checkOutButton(ActionEvent event) {
 
-        String logInuser = Control.getLogInUser();
+        logInuser = Control.getLogInUser();
         String payAmount = subtotalLabel.getText();
         dbQueries.addOrderRecord(logInuser, payAmount);
+        getOrderId = dbQueries.getOrderId();
         for (CartTableModel person : cartTableView.getItems()) {
             String productName = person.getName();
             dbQueries.updateStockQuantity(productName);
         }
-        ReceiptPrinter(cartTableView);
+        generateReciept();
         AlertBox("Order Place Successfully!");
         dbQueries.deleteCart();
         backToMainWindow();
@@ -124,28 +130,87 @@ public class PaymentController extends DbConnector implements Initializable {
     }
 
 
-    public void ReceiptPrinter(Node node) {
+    public void generateReciept() {
+        Rectangle pagesize = new Rectangle(400, 861);
+        Document doc = new Document(PageSize.HALFLETTER, 0f, 0f, 10f, 0f);
+        BaseFont arialUnicodeMs;
+        try {
+            Font font1 = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD);
+            Font font2 = new Font(Font.FontFamily.COURIER, 12, Font.NORMAL);
+            Font font3 = new Font(Font.FontFamily.COURIER, 12, Font.BOLD);
+            PdfWriter.getInstance(doc, new FileOutputStream("src/RecieptRecord/reciept_" + getOrderId + ".pdf"));
+            doc.open();
+            Paragraph p1 = new Paragraph("************************************", font1);
+            Paragraph p2 = new Paragraph("FoodieHeroes Store\nBill Reciept\n", font1);
+            Paragraph p3 = new Paragraph("************************************\n", font1);
+            Paragraph p4 = new Paragraph("   CustomerId : " + logInuser + "\n    OrderId    : " + getOrderId + "\n\n", font3);
+            PdfPTable table = new PdfPTable(3);
+            PdfPCell c1Title = new PdfPCell(new Paragraph("Product Name", font1));
+            PdfPCell c2Title = new PdfPCell(new Paragraph("Quantity", font1));
+            PdfPCell c3Title = new PdfPCell(new Paragraph("Price", font1));
+            c1Title.setHorizontalAlignment(Element.ALIGN_LEFT);
+            c2Title.setHorizontalAlignment(Element.ALIGN_RIGHT);
+            c3Title.setHorizontalAlignment(Element.ALIGN_RIGHT);
+            c1Title.setBorder(Rectangle.NO_BORDER);
+            c2Title.setBorder(Rectangle.NO_BORDER);
+            c3Title.setBorder(Rectangle.NO_BORDER);
 
-        Printer printer = Printer.getDefaultPrinter();
+            p1.setAlignment(Element.ALIGN_CENTER);
+            p2.setAlignment(Element.ALIGN_CENTER);
+            p3.setAlignment(Element.ALIGN_CENTER);
 
-        PageLayout page = printer.getDefaultPageLayout();
+            table.addCell(c1Title);
+            table.addCell(c2Title);
+            table.addCell(c3Title);
 
-        double X = page.getPrintableWidth() / (node.getBoundsInParent().getWidth());
-        double Y = page.getPrintableHeight() / (node.getBoundsInParent().getHeight());
 
-        Scale SCL = new Scale(X, Y);
-        node.getTransforms().add(SCL);
+            doc.add(p1);
+            doc.add(p2);
+            doc.add(p3);
+            doc.add(p4);
 
-        PrinterJob PJ = PrinterJob.createPrinterJob(printer);
 
-        if (PJ != null) {
-            boolean Success = PJ.printPage(page, node);
-            if (Success) {
-                PJ.endJob();
+            String sql = "Select name ,quantity, Price from meals;";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    while (resultSet.next()) {
+                        PdfPCell c1 = new PdfPCell(new Paragraph(resultSet.getString("name"), font2));
+                        PdfPCell c2 = new PdfPCell(new Paragraph(resultSet.getString("quantity"), font2));
+                        PdfPCell c3 = new PdfPCell(new Paragraph(resultSet.getString("Price"), font2));
+                        c2.setHorizontalAlignment(Element.ALIGN_RIGHT);
+                        c3.setHorizontalAlignment(Element.ALIGN_RIGHT);
+                        c1.setBorder(Rectangle.NO_BORDER);
+                        c2.setBorder(Rectangle.NO_BORDER);
+                        c3.setBorder(Rectangle.NO_BORDER);
+                        table.addCell(c1);
+                        table.addCell(c2);
+                        table.addCell(c3);
+
+                    }
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, e);
             }
+
+            doc.add(table);
+            Paragraph p5 = new Paragraph(" \n\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  ", font1);
+            p5.setAlignment(Element.ALIGN_CENTER);
+            Paragraph totalBill = new Paragraph("\nTotal Bill : " + subtotalLabel.getText() + "        ", font1);
+            totalBill.setAlignment(Element.ALIGN_RIGHT);
+
+            doc.addCreator("zain_335");
+            doc.addHeader("FoodieStore", "FooodieHeroes");
+
+            doc.add(p5);
+            doc.add(totalBill);
+            doc.close();
+            AlertBox("reciept_" + getOrderId + "  is Successfully Generated to RecieptRecord Directoy");
+        } catch (Exception e) {
+            AlertBox(e + "");
         }
-        node.getTransforms().remove(SCL);
+
     }
+
 
     public void deleteFromShopCartList(ActionEvent event) {
         dbQueries.deleteCart();
